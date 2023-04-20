f077f5951f559bd7d9b4f1b0b33b60dcff9cbe49
David Ngomane <70548142+Dngomane@users.noreply.github.com>
2023-04-20 13:44:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+//import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [delimiter, setDelimiter] = useState(',');
+  const [databaseUrl, setDatabaseUrl] = useState('');
+  const [databaseRef, setDatabaseRef] = useState(null);
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+  }
+
+  const handleDelimiterChange = (event) => {
+    const selectedDelimiter = event.target.value;
+    setDelimiter(selectedDelimiter);
+  }
+
+  const handleDatabaseUrlChange = (event) => {
+    const selectedDatabaseUrl = event.target.value;
+    setDatabaseUrl(selectedDatabaseUrl);
+
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp({
+        databaseURL: selectedDatabaseUrl
+      });
+    }
+
+    const databaseReference = firebase.database().ref();
+    setDatabaseRef(databaseReference);
+  }
+
+  const handleInsertData = () => {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const fileContents = event.target.result;
+      const rows = fileContents.split('\n');
+
+      rows.forEach((row) => {
+        const fields = row.split(delimiter);
+
+        // Insert data into Firebase Realtime Database
+        const dataToInsert = {
+          field1: fields[0],
+          field2: fields[1],
+          field3: fields[2]
+        };
+
+        databaseRef.push(dataToInsert)
+          .then(() => {
+            console.log('Data inserted successfully');
+          })
+          .catch((error) => {
+            console.error('Error inserting data:', error);
+          });
+      });
+    }
+
+    reader.readAsText(file);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <input type="text" value={delimiter} onChange={handleDelimiterChange} />
+      <input type="text" value={databaseUrl} onChange={handleDatabaseUrlChange}
+      />
+<button onClick={handleInsertData}>Insert Data</button>
+</div>
+);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//import firebase from 'firebase';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9lbwvBZxJOHuoTSlnWI6N1ytFS-VKoJM",
+  authDomain: "fisa-90e90.firebaseapp.com",
+  projectId: "fisa-90e90",
+  storageBucket: "fisa-90e90.appspot.com",
+  messagingSenderId: "73733762599",
+  appId: "1:73733762599:web:f6388eec42f776fd20e802"
+};
+
+firebase.initializeApp(firebaseConfig)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
